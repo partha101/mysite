820bc5d6f8182473fe02ad678228983ba3a60eb1
partha101 <thakuriapartha780@gmail.com>
2022-11-21 01:11:30 -0800

    Successfully implemented the view for the shortest route
    Did initial implementation of showing interactive map with shortest route in django. Used osmnx library functions to find the shortest path.

--- maps/views.py
+++ maps/views.py
@@ -25,8 +25,6 @@ def show(request):
         from IPython.display import IFrame
     except:
         raise Http404("Failed to import modules")
-    filepath = "maps/graph.html"
-    # return render(request, filepath, {})
 
     list = [request.POST.get("handle", None),request.POST.get("handle2", None)]
     input1 = list[0]
@@ -39,25 +37,19 @@ def show(request):
         X2,Y2 = B[0],B[1]
     if (isfloat(X1) and isfloat(Y1) and isfloat(X2) and isfloat(Y2)):
         X1,Y1,X2,Y2 = float(X1),float(Y1),float(X2),float(Y2)
-        # G = ox.graph_from_place("California, USA", network_type="drive")
 
         graph_path = "maps/test.osm"
         G = ox.graph_from_xml(graph_path, retain_all=True)
-        # plot the street network with folium
-        # m1 = ox.plot_graph_folium(G, popup_attribute="name", weight=2, color="#8b0000")
-        # filepath = "D:\graph.html"
-        # m1.save(filepath)
         orig = ox.distance.nearest_nodes(G, X=X1 , Y=Y1)
         dest = ox.distance.nearest_nodes(G, X =X2 , Y=Y2)
         route = nx.shortest_path(G, orig, dest)
-        if type(route) == type(list):
-            exit(1)
-        m2 = ox.plot_route_folium(G, route, weight=10)
-        filepath = "maps/route.html"
+        m2 = ox.plot_route_folium(G, route, color = 'red', dashed_array = 10)
+        filepath = "D:/route.html"
         m2.save(filepath)
-        # import shutil
+        import shutil
 
-        # shutil.move('D:/route.html', 'maps/route.html')
+        filepath = 'maps/route.html'
+        shutil.move('D:/route.html', 'D:/Spatial-Project/mysite/maps/templates/maps/route.html')
         return render(request, filepath, {})
     else:
         return render(request, 'maps/index.html', {'list': list, 'flag':1})
