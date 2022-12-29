f05aa02264ab37a5e353168c2b6f69296a057ee4
partha101 <thakuriapartha780@gmail.com>
2022-12-29 03:05:01 -0800

    Cleaned up the code

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('maps/', include('maps.urls')),
+    path('', include('maps.urls')),
     path('admin/', admin.site.urls),
 ]
